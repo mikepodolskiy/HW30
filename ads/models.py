a20d9614a28696bbcedaf126f124f7a6ad83fc71
mikepodolskiy <mikepodolskiy@gmail.com>
2023-05-13 12:51:41 +0300

    step 3 done, convert function finished

--- ads/models.py
+++ ads/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-ADC
 
-id
-name
-author
-price
-description
-addres
-is_published
+class Ads(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True)
+    name = models.CharField(max_length=140)
+    author = models.CharField(max_length=140)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.CharField(max_length=2000)
+    address = models.CharField(max_length=280)
+    is_published = models.BooleanField()
 
-Categories
 
-id
-name
-
+class Categories(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True)
+    name = models.CharField(max_length=140)
--- data/utils.py
+++ data/utils.py
@@ -2,15 +2,41 @@ import csv
 import json
 
 
-def csv_json_converter(csv_file_path, json_file_path):
+def csv_json_converter(csv_file_path, json_file_path, model_path):
+    """
+    Function for converting csv to json (as django required) steps depends on differences in initial data
+    In this case differences are: format no for Django, line breakers in text, wrong register
+    :param csv_file_path: path to file with initial data
+    :param json_file_path: path to json file
+    :param model_path:name that model should have
+    :return: -
+    """
     csv_file = csv.DictReader(open(csv_file_path, 'r', encoding='utf-8'))
+
     json_list = [row for row in csv_file]
+    upd_data = []
+    for item in json_list:
+        new_item = {k.lower(): v.replace("\n", " ") for k, v in item.items()}
+        upd_data.append(new_item)
 
+    bool_upd_data = []
+    for item in upd_data:
+        if "is_published" in item.keys():
+            if item["is_published"] == "TRUE":
+                item["is_published"] = True
+            else:
+                item["is_published"] = False
+            bool_upd_data.append(item)
+        else:
+            bool_upd_data = upd_data
+
+    django_format_data = []
+    for item in bool_upd_data:
+        new_item = {"pk": item["id"], "model": model_path, "fields": {k: v for k, v in item.items() if k != "id"}}
+        django_format_data.append(new_item)
     with open(json_file_path, "w", encoding="utf-8") as file:
-        json.dump(json_list, file, ensure_ascii=False)
-
-    return "File converted"
+        json.dump(django_format_data, file, ensure_ascii=False)
 
 
-csv_json_converter('./datasets/ads.csv', './json/ads.json')
-csv_json_converter('./datasets/categories.csv', './json/categories.json')
+csv_json_converter('./datasets/ads.csv', '../fixtures/ads.json', 'ads.ads')
+csv_json_converter('./datasets/categories.csv', '../fixtures/categories.json', 'ads.categories')
